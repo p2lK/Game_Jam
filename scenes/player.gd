9baa73fb7d9a4ec6fe7ff816d8a73b2020b8a7f3
Guzel <pokelau2003@gmail.com>
2023-05-10 18:07:44 +0200

    added basic movement and animnation

---
+++ scenes/player.gd
@@ -0,0 +1,28 @@
+extends CharacterBody2D
+
+@onready var sprite = $midas_lennox
+
+var speed = 300.0
+
+func _physics_process(delta):
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var dir = Vector2(0, 0)
+	
+	dir.x = Input.get_action_strength('right') - Input.get_action_strength('left')
+	dir.y = Input.get_action_strength('down') - Input.get_action_strength('up')
+	
+	if dir.x < 0:
+		sprite.scale.x = -1
+	elif dir.x > 0:
+		sprite.scale.x = 1
+	
+	if dir == Vector2(0, 0):
+		sprite.play('idle')
+	else:
+		sprite.play('walk')
+	
+	
+	velocity = dir * speed
+	
+	move_and_slide()
