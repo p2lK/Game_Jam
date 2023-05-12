e8eb3899848b0963a9acc6c6c2c0be1d3fa77479
Guzel <pokelau2003@gmail.com>
2023-05-12 19:39:33 +0200

    added monologue

--- Scenes/orange_hair.gd
+++ Scenes/orange_hair.gd
@@ -7,7 +7,7 @@ func _process(delta):
 	label.visible = player_detector.player_in_this
 
 func _on_player_detector_interacted_with():
-	label.text = 'placeholder text'
+	label.text = 'Immer dieses unfähige Personal... wie oft muss ich mich noch vorstellen. Mr. MacClellan, bitteschön!'
 
 func _on_player_detector_player_exited():
-	label.text = 'press space to interact'
+	label.text = 'E zum interagieren'
